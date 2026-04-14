6f628050fbcdf2ae2037694072d85f3f9dde8030
Martim Ramalho <tim.ramalho98@gmail.com>
2026-04-14 12:54:22 +0100

    initial scaffold from Spring Initializr

---
+++ src/main/java/com/farmacia/scheduler/PharmacySchedulerApplication.java
@@ -0,0 +1,13 @@
+package com.farmacia.scheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PharmacySchedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PharmacySchedulerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/farmacia/scheduler/PharmacySchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.farmacia.scheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PharmacySchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
